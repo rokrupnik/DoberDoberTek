8a767c7e6398ca32bc99fb622180fc08f2d00e74
rokrupnik <rok7rupnik@gmail.com>
2016-08-06 11:09:37 +0200

    Added buttons for hunger levels and some other fixes.

--- src/js/index.js
+++ src/js/index.js
@@ -32,7 +32,7 @@ var app =
         //app.loadData();
     },
 
-    hungerLevel: m.prop(''),
+    hungerLevel: m.prop('srednje'),
     vv: m.prop(0),
     iv: m.prop(0),
     pp: m.prop(0),
@@ -42,33 +42,6 @@ var app =
         // View model
         vm: {
             init: function () {
-            },
-
-            enableLinkButtons: function (element, isInitialized) {
-                if (app.hungerLevel().length > 0) {
-                    // Set active class on pressed button
-                    $('button.btn').removeClass('active');
-                    switch (app.hungerLevel()) {
-                        case 'malo': {
-                            $('button.btn-success').addClass('active');
-                            break;
-                        }
-                        case 'srednje': {
-                            $('button.btn-warning').addClass('active');
-                            break;
-                        }
-                        case 'zelo': {
-                            $('button.btn-danger').addClass('active');
-                            break;
-                        }
-                    }
-
-                    // Enable link buttons
-                    if ($('button.link').prop('disabled')) {
-                        $('button.link').prop('disabled', false);
-                    }
-                }
-
             },
 
             selectText: function (e) {
@@ -90,32 +63,41 @@ var app =
             return m("div.input-container.row", [
                 m("div.input-group.col-xs-12", [
                     m("h1.text-center.col-xs-12", "Kako lačni ste?"),
-                    m("div.btn-group.col-xs-12", [
-                        m("button.btn.btn-success.col-xs-4", {onclick: m.withAttr("value", app.hungerLevel), value: "malo", config: app.input.vm.enableLinkButtons}, m("small", "Skoraj siti")),
-                        m("button.btn.btn-warning.col-xs-4", {onclick: m.withAttr("value", app.hungerLevel), value: "srednje", config: app.input.vm.enableLinkButtons}, m("small", "Nič posebnega")),
-                        m("button.btn.btn-danger.col-xs-4", {onclick: m.withAttr("value", app.hungerLevel), value: "zelo", config: app.input.vm.enableLinkButtons}, m("small", "Za vola pojest!"))
+                    m("div.col-xs-12", [
+                        m("span.padless.col-xs-4", [
+                            m("input#malo", {type: "radio", name: "hungerLevel", onchange: m.withAttr("value", app.hungerLevel), value: "malo"}, m("small", "Skoraj siti")),
+                            m("label.malo.text-center", {for: "malo"}, m("small", "Skoraj siti")),
+                        ]),
+                        m("span.padless.col-xs-4", [
+                            m("input#srednje", {type: "radio", name: "hungerLevel", onchange: m.withAttr("value", app.hungerLevel), value: "srednje", checked: true}, m("small", "Nič posebnega")),
+                            m("label.srednje.text-center", {for: "srednje"}, m("small", "Nič posebnega")),
+                        ]),
+                        m("span.padless.col-xs-4", [
+                            m("input#zelo", {type: "radio", name: "hungerLevel", onchange: m.withAttr("value", app.hungerLevel), value: "zelo"}, m("small", "Za vola pojest!")),
+                            m("label.zelo.text-center", {for: "zelo"}, m("small", "Za vola pojest!"))
+                        ]),
                     ]),
                 ]),
                 m("div.input-group.col-xs-12", [
                     m("h1.col-xs-12.text-center", "Koliko vas je?"),
-                    m("strong.input-label.text-center.col-xs-4", "VV/BB 11-"),
-                    m("strong.input-label.text-center.col-xs-4", "IV 11-15"),
-                    m("strong.input-label.text-center.col-xs-4", "PP/SKVO 16+"),
+                    m("input-label.text-center.col-xs-4", "VV/BB 11-"),
+                    m("input-label.text-center.col-xs-4", "IV 11-15"),
+                    m("input-label.text-center.col-xs-4", "PP/SKVO 16+"),
                     m("input.col-xs-4#vv", {type: "number", onchange: m.withAttr("value", app.vv), onclick: app.input.vm.selectText, value: app.vv()}),
                     m("input.col-xs-4#iv", {type: "number", onchange: m.withAttr("value", app.iv), onclick: app.input.vm.selectText, value: app.iv()}),
                     m("input.col-xs-4#pp", {type: "number", onchange: m.withAttr("value", app.pp), onclick: app.input.vm.selectText, value: app.pp()})
                 ]),
                 m("div.col-xs-12", [
-                    m("button.btn.btn-primary.btn-lg.link.col-xs-4 col-xs-offset-1", {onclick: m.withAttr("value", app.utils.changeRoute), value: "/zivila", disabled: true}, "ŽIVILA"),
-                    m("button.btn.btn-primary.btn-lg.link.col-xs-4 col-xs-offset-2", {onclick: m.withAttr("value", app.utils.changeRoute), value: "/jedi", disabled: true}, "JEDI")
+                    m("button.link.col-xs-4 col-xs-offset-1", {onclick: m.withAttr("value", app.utils.changeRoute), value: "/zivila"}, "ŽIVILA"),
+                    m("button.link.col-xs-4 col-xs-offset-2", {onclick: m.withAttr("value", app.utils.changeRoute), value: "/jedi"}, "JEDI")
                 ]),
-                m("div.col-xs-12", [
+                /*m("div.col-xs-12", [
                     m("a.btn.btn-info.col-xs-6 col-xs-offset-3", {href: "https://docs.google.com/forms/d/1v5eekn6bUch71Ct5GZ_1H-Up9p1HssnqPdoDeAGlfCc/viewform?c=0&w=1", target: "_blank"}, [
                         m("span", "Oddaj predlog"),
                         m("br"),
                         m("span", "za izboljšavo"),
                     ]),
-                ]),
+                ]),*/
                 // m("div.col-xs-3.text-center", app.hungerLevel()), 
                 // m("div.col-xs-3.text-center", app.vv()), 
                 // m("div.col-xs-3.text-center", app.iv()), 
